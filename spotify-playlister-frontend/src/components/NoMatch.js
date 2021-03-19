d06f8c87bb04549e605466828ddabb75e25c7a81
Alexander M. Clamor <alexander.clamor@gmail.com>
2021-03-19 09:01:21 -0700

    add css to NoMatch component

--- spotify-playlister-frontend/src/components/NoMatch.js
+++ spotify-playlister-frontend/src/components/NoMatch.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
+import styles from './NoMatch.module.css'
 
 const NoMatch = () => {
     
     return (
-        <div>
-          <h1>Sorry - It seems that page doesn't exist</h1>
-          <Link to='/'>Go to main page</Link>
+        <div className={styles.container}>
+          <h2 className={styles['error-code']}>404</h2>
+          <h2 className={styles['error-msg']}> Page not found</h2>
+          <h5 className={styles['homepage-link-box']}><Link className={styles['homepage-link']} to='/'>Go to Main Page</Link></h5>
         </div>
     )
 }
